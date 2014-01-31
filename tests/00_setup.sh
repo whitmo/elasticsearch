677249d373c41e301a455f265e46d6f6ad53efdb
Charles Butler <chuck@dasroot.net>
2014-01-31 11:39:26 -0500

    Removes test_config per bug 1274966 and updates tests

--- tests/00_setup.sh
+++ tests/00_setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-
-sudo apt-get install install amulet python-requests
-
+sudo apt-get install python-setuputils -y                                                             
+                                                                                                      
+sudo apt-add-repository ppa:juju/stable -y                                                            
+sudo apt-get update                                                                                   
+                                                                                                      
+#I have no idea if this is running via python2 or python3, so fetch both                              
+# copies of the requests lib                                                                          
+sudo apt-get install juju juju-local amulet python3-requests python-requests -y 
